d214d74031159266f68f1c3427800fd1ad0f157c
zabronax <zabronax@gmail.com>
2025-06-25 20:41:55 +0200

    define initial development environments

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "An experiment into holistic monorepo management";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      withSystem = nixpkgs.lib.genAttrs [
+        "x86_64-linux" # TODO! NOT Verified
+        "x86_64-darwin" # TODO! NOT Verified
+        "aarch64-linux" # TODO! NOT Verified
+        "aarch64-darwin" # Verified
+      ];
+      withPkgs = callback: withSystem (system: callback (import nixpkgs { inherit system; }));
+    in
+    {
+      devShells = withPkgs (pkgs: rec {
+        default = fullDevShell;
+
+        fullDevShell = pkgs.mkShell {
+          packages = with pkgs; [
+          ];
+        };
+      });
+
+      formatter = withPkgs (pkgs: pkgs.nixfmt-rfc-style);
+    };
+}
